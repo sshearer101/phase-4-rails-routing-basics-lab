524162a14f91c1a9b7342a50587b5d3959a46b1e
Sam Shearer <sshearer101@gmail.com>
2021-09-19 15:58:18 -0600

    commit tree 600ecd8eb9345f237ae2e27868ec2c3eb5a04ad4

---
+++ app/controllers/student_controller.rb
@@ -0,0 +1,16 @@
+class StudentController < ApplicationController
+    def index
+        students = Student.all
+        render json: students
+      end
+    
+      def grades
+        students = Student.order(grade: :desc)
+        render json: students
+      end
+    
+      def highest_grade
+        student = Student.order(grade: :desc).first
+        render json: student
+      end
+end
